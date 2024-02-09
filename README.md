6fa52ea8579365e1b732a09894d49e45e1eb84c5
mr <marco.rondina@studenti.polito.it>
2024-02-09 15:00:27 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# haiking-hai2324
+# Sample of a Web-based Voice User Interface (in Python)
+
+This repository is an example for getting started with voice user interfaces.
+
+It uses the Web Speech API (experimental) for speech-to-text and text-to-speech operations. For Natural Language Processing, it uses [Dialogflow ES](https://dialogflow.cloud.google.com) agent, included as a `.zip` file.
+
+Tested in Chrome 110+, Firefox 109+, and Safari 16+. Please, notice that speech-to-text capability might not work in Safari and Firefox.
+
+To get started with the project, import the `WeatherAgent.zip` into Dialogflow, generate a _service key_ from Google (in JSON), and download it into the root of the project as `client-secret.json`.
---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template
+from google.oauth2 import service_account
+from google.cloud import dialogflow
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    # just render the HTML homepage
+    return render_template("index.html")
+
+
+@app.route('/process', methods=['POST'])
+def detect_intent():
+    # create a new session
+    credentials = service_account.Credentials.from_service_account_file("haii-finalproject-e117566d75c1.json")
+    session_client = dialogflow.SessionsClient(credentials=credentials)
+
+    # replace the project id, since it is fake
+    # 123456 is the session id, which should not be changed within an entire conversation with a single user
+    session = session_client.session_path("haii-finalproject", 123456)
+
+    # prepare a new query input with the inner text, taken from the POST request
+    text_input = dialogflow.TextInput(text=request.form["message"], language_code="en-US")
+    query_input = dialogflow.QueryInput(text=text_input)
+
+    # send the request and get the response
+    response = session_client.detect_intent(session=session, query_input=query_input)
+
+    # print(response.query_result.intent.displayName)
+    # print(response.query_result.parameters['geo-city'])
+
+    # reply to the POST request: the textual response is in "fullfilment"
+    return response.query_result.fulfillment_text
+
+if __name__ == '__main__':
+    app.run()
